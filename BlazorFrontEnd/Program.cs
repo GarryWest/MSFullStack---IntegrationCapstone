54204304aaa10fdd486acd2cbd308f629c4c5e5c
GarryWest <garrywest@msn.com>
2025-04-11 11:31:27 -0500

    Integrated and debugged

--- BlazorFrontEnd/Program.cs
+++ BlazorFrontEnd/Program.cs
@@ -1,3 +1,6 @@
+// BlazorFrontEnd/Program.cs
+// This file is part of the BlazorFrontEnd project.
+
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorFrontEnd;
@@ -6,6 +9,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5237") });
 
 await builder.Build().RunAsync();
--- MinimalBackend/Program.cs
+++ MinimalBackend/Program.cs
@@ -1,6 +1,25 @@
+// MinimalBackend/Program.cs
+// This file is part of the MinimalBackend project.
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(); // Add CORS services
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Add this line to enable CORS
+app.UseCors(policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+app.MapGet("/api/products", () =>
+{
+    return new[]
+    {
+        new { Id = 1, Name = "Laptop", Price = 1200.50, Stock = 25 },
+        new { Id = 2, Name = "Headphones", Price = 50.00, Stock = 100 }
+    };
+});
+
+// Apply CORS globally
+app.UseRouting();
 
 app.Run();
